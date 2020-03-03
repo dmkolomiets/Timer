a5a97849d8b2664ec78b1eafec0318a7f2b4192b
Dmitry Kolomiets <60270069+dmkolomiets@users.noreply.github.com>
2020-03-03 18:37:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+
+window.addEventListener('DOMContentLoaded', function() { 
+
+    'use strict';
+
+    
+
+    let deadLine = '2020-03-03';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };     
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            
+            timeInterval = setInterval(updateClock, 1000);
+
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if(num < 10 && num > -1) {
+                    return '0' + num;
+                } else {
+                    return num;
+                }
+            }
+            
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = addZero('0');
+                minutes.textContent = addZero('0');
+                seconds.textContent = addZero('0');
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
+
+});
